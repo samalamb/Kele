61aad9df176e5dc24944505a7aecaf30e510cdcd
Sam Dieck <samuel.dieck@gmail.com>
2016-05-26 15:37:43 -0700

    Initial commit and checkpoint 1

---
+++ lib/kele.rb
@@ -0,0 +1,8 @@
+class Kele
+  require HTTParty
+
+  def initialize(username, password)
+    @base_url = 'https://www.bloc.io/api/v1'
+    @auth_token = self.class.post(base_url, username, password)
+  end
+end
