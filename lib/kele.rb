02cfa92f094530b2d04d6db91fd6c912a1db1f50
Sam Dieck <samuel.dieck@gmail.com>
2016-05-27 16:21:23 -0700

    Added function to retireve mentor availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -5,7 +5,7 @@ class Kele
   include HTTParty
 
   def initialize(username, password)
-    response = self.class.post(base_api_endpoint("sessions"), body: {email: username, password: password})
+    response = self.class.post(base_api_endpoint('/sessions'), body: {email: username, password: password})
     @auth_token = response["auth_token"]
     raise StandardError.new('Username or Password are Incorrect') unless @auth_token
   end
@@ -13,6 +13,12 @@ class Kele
   def get_me
     response = self.class.get(base_api_endpoint('/users/me'), headers: { "authorization" => @auth_token })
     @current_user = JSON.parse(response.body)
+  end
+
+  # mentor_id = 2290632
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(base_api_endpoint("/mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+    @mentor_availability = JSON.parse(response.body)
   end
 
 private
