4b625067fae64f0ee164d3568778e8c83a4d7b45
Sam Dieck <samuel.dieck@gmail.com>
2016-05-27 15:04:28 -0700

    Initialization method and error raiser when invalid credentials are met

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,8 +1,15 @@
+require 'httparty'
+
 class Kele
   include HTTParty
 
   def initialize(username, password)
-    @base_url = 'https://www.bloc.io/api/v1'
-    @auth_token = self.class.post(base_url, username, password)
+    response = self.class.post(base_api_endpoint("sessions"), body: {email: username, password: password})
+    @auth_token = response["auth_token"]
+    raise StandardError.new('Username or Password are Incorrect') unless @auth_token
+  end
+
+  def base_api_endpoint(end_point)
+    "https://www.bloc.io/api/v1/#{end_point}"
   end
 end
